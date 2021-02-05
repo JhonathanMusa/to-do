84d912ff5d49b0f41aa185cf6ef250753abaea0a
jhonathanmusa <jhonathansamu91@gmail.com>
2021-02-05 17:46:00 -0500

    adding new elements

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-for (var i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+const myNodelist = document.getElementsByTagName("LI");
+for (let i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement("SPAN");
+  let spanTxt = document.createTextNode("\u00D7");
   span.className = "close";
-  span.appendChild(txt);
+  span.appendChild(spanTxt);
   myNodelist[i].appendChild(span);
 }
 
 // click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-for (var i = 0; i < close.length; i++) {
+const close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
   close[i].onclick = function () {
-    var div = this.parentElement;
+    let div = this.parentElement;
     div.style.display = "none";
   };
 }
 
 // Add a "checked" symbol when clicking on list item
-var list = document.querySelector("ul");
+const list = document.querySelector("ul");
 list.addEventListener(
   "click",
-  function (ev) {
-    if (ev.target.tagName === "LI") {
-      ev.target.classList.toggle("checked");
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
     }
   },
   false
@@ -31,10 +31,10 @@ list.addEventListener(
 
 // Create a new list item when clickin on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let liText = document.createTextNode(inputValue);
+  li.appendChild(liText);
   if (inputValue === "") {
     alert("You must write something");
   } else {
@@ -42,15 +42,15 @@ function newElement() {
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  const span = document.createElement("SPAN");
+  const spanTxt = document.createTextNode("\u00D7");
   span.className = "close";
-  span.appendChild(txt);
+  span.appendChild(spanTxt);
   li.appendChild(span);
 
-  for (var i = 0; i < close.length; i++) {
+  for (let i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     };
   }
